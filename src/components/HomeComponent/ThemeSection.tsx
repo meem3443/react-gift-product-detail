2acf288c3694884908cdfcd44770cd137db6393a
meem3443 <jinsr0235@gmail.com>
2025-07-31 16:21:44 +0900

    chore : remove useless comment

--- src/components/HomeComponent/ThemeSection.tsx
+++ src/components/HomeComponent/ThemeSection.tsx
@@ -1,7 +1,7 @@
 import { ThemeCard } from "./Cards/ThemeCard";
 import { getThemes, type Theme } from "../../api/theme";
 import { useNavigate } from "react-router-dom";
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 
 export const ThemeSection = () => {
   const navigate = useNavigate();
@@ -10,44 +10,11 @@ export const ThemeSection = () => {
     navigate(`/themes/${themeId}/products`);
   };
 
-  const {
-    data: themes,
-    isLoading,
-    isError,
-    error,
-  } = useQuery<Theme[], Error>({
+  const { data: themes } = useSuspenseQuery<Theme[], Error>({
     queryKey: ["themes"],
     queryFn: getThemes,
     staleTime: 1000 * 5,
   });
-
-  if (isLoading) {
-    return (
-      <div className="bg-white w-full h-auto p-5 pb-10 text-center">
-        <p className="text-xl pt-5 pb-5 pl-3 font-bold">선물 테마</p>
-        <p>테마 로딩 중...</p>
-      </div>
-    );
-  }
-
-  if (isError) {
-    return (
-      <div className="bg-white w-full h-auto p-5 pb-10 text-center text-red-500">
-        <p className="text-xl pt-5 pb-5 pl-3 font-bold">선물 테마</p>
-
-        <p>오류 발생: {error?.message || "알 수 없는 오류"}</p>
-      </div>
-    );
-  }
-
-  if (!themes || themes.length === 0) {
-    return (
-      <div className="bg-white w-full h-auto p-5 pb-10 text-center text-gray-600">
-        <p className="text-xl pt-5 pb-5 pl-3 font-bold">선물 테마</p>
-        <p>표시할 테마가 없습니다.</p>
-      </div>
-    );
-  }
 
   return (
     <div className="bg-white w-full h-auto p-5 pb-10 ">
